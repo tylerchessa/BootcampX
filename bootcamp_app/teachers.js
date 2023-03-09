37c11d0bbd9045fe8d47093fb53f43cfc4153815
tylerchessa <tylerchessa@gmail.com>
2023-03-09 05:04:05 +0000

    parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const firstInput = process.argv[2] || 'JUL02'
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, [firstInput])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
